c8c25a3de74dcfa0d61cffe006c54dd9e5274e5a
BharatRohit <94052041+BharatRohit@users.noreply.github.com>
2023-01-19 17:01:13 +0530

    Update bevy.py

--- bevy.py
+++ bevy.py
@@ -1,16 +1,8 @@
-# import streamlit as st
-# import pandas as pd
-# import matplotlib.pyplot as plt
-# columns = ["Flow_ls", "Head_m"]
-# df = pd.read_csv("CSV.csv", usecols=columns)
-# print("contents in csv file:", df)
-# plt.plot(df.Flow_ls, df.Head_m)
-# plt.show()
 import streamlit as st
-import numpy as np
-
-arr = np.random.normal(1, 1, size=100)
-fig, ax = plt.subplots()
-ax.hist(arr, bins=20)
-
-st.pyplot(fig)
+import pandas as pd
+import matplotlib.pyplot as plt
+columns = ["Flow_ls", "Head_m"]
+df = pd.read_csv("CSV.csv", usecols=columns)
+print("contents in csv file:", df)
+plt.plot(df.Flow_ls, df.Head_m)
+plt.show()
