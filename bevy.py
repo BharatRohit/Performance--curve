ff9fc938a3ece1fb0065ee6dc5c2bba8223f84d2
BharatRohit <94052041+BharatRohit@users.noreply.github.com>
2023-01-23 17:07:26 +0530

    Update bevy.py

--- bevy.py
+++ bevy.py
@@ -1,10 +1,13 @@
 import streamlit as st
 import pandas as pd
+
 import matplotlib.pyplot as plt
 columns = ["Flow_ls", "Head_m"]
 df = pd.read_csv("CSV.csv", usecols=columns)
 print("contents in csv file:", df)
 plt.plot(df.Flow_ls, df.Head_m)
+
+# option = st.selectbox('Select pump model',(x1,x2,x3)) 
 plt.show()
-st.pyplot()
+st.area_chart()
 st.set_option('deprecation.showPyplotGlobalUse', False)
